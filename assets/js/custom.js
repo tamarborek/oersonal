6edbfc65c2a752d17c3a67ed24a6b1c33748903d
Tamar Borek <39298358+tamarborek@users.noreply.github.com>
2022-12-11 11:25:37 +0200

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,94 @@
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+       
+        scroll_fun:function()
+        {
+            //SCROLLING SCRIPTS
+
+            $('.move-me a').bind('click', function (event) { //just pass 'move-me' in div and start scrolling between links
+                var $anchor = $(this);
+                $('html, body').stop().animate({
+                    scrollTop: $($anchor.attr('href')).offset().top
+                }, 1200, 'easeInOutExpo');
+                event.preventDefault();
+
+                //CLOSING MENU AFTER CLICK
+                $('#side-menu').animate({ left: '-250px', top: '-1000px' });
+                
+            });
+
+        },
+        sidemenu_fun:function()
+        {
+            //SIDE MENU SCRIPTS
+
+            $('.menu-close-icon').click(function (e) {
+                e.preventDefault();
+                $('#side-menu').animate({ left: '-250px', top: '-1000px' });
+               
+            });
+            $('.menu-open-icon').click(function (e) {
+                e.preventDefault();
+                $('#side-menu').animate({ left: '0px', top: '0px' });
+               
+            });
+
+        },
+        socialmenu_fun:function()
+        {
+            //SOCIAL MENU SCRIPTS
+
+            $('.menu-open-social').click(function (e) {
+                e.preventDefault();
+                $('.social-menu').animate({ right: '0px'});
+            });
+            $('.close-social').click(function (e) {
+                e.preventDefault();
+                $('.social-menu').animate({ right: '-340px' });
+            });
+
+
+        }, 
+        nicescroll_fun: function () {
+            $("html").niceScroll();
+        },
+        custom_fun:function()
+        {
+
+
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.scroll_fun();
+        mainApp.sidemenu_fun();
+        mainApp.socialmenu_fun();
+        mainApp.nicescroll_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
+
+
